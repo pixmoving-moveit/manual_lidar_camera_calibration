c754a39c260a0f16a08e1823b583b18c541f38a0
ahuazuipiaoliang <mark@pixmoving.net>
2024-07-24 14:10:48 +0800

    feat: update roslogger in extrinsic_calibrator

--- scripts/extrinsic_calibrator.py
+++ scripts/extrinsic_calibrator.py
@@ -13,27 +13,7 @@ from std_msgs.msg import Float32MultiArray
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 
 
-def extrinsicsCompute(object_points_list: list, image_points_list: list, k_matrix: list):
-  object_points = np.array(object_points_list).reshape(len(object_points_list), 3, 1)
-  image_points = np.array(image_points_list).reshape(len(object_points_list), 2, 1)
-  camera_matrix = np.array(k_matrix).reshape(3, 3)
-  dist_coeffs = np.zeros((5,1))
-  ret_val, r_vec, t_vec = cv2.solvePnP(object_points, image_points, camera_matrix, dist_coeffs)
-  if(ret_val):
-    r_solved, _ = cv2.Rodrigues(r_vec)
-    t_vec = t_vec.reshape(3, 1)
-    homo_vec = np.array([[0.0, 0.0, 0.0, 1.0]])
-    tf_matrix = np.hstack((r_solved, t_vec))
-    tf_matrix = np.vstack((tf_matrix, homo_vec))
-    print("transformation matrix:")
-    print(tf_matrix)
-    r = R.from_matrix(r_solved)
-    quat = r.as_quat()
-    print("quaternion: "+"[x, y, z, w]")
-    print(quat)
-    euler = r.as_euler('zyx')
-    print(f"x: {t_vec[0][0]}\ny: {t_vec[1][0]}\nz: {t_vec[2][0]}\nroll: {euler[2]}\npitch: {euler[1]}\nyaw: {euler[0]}")
-    return quat, t_vec
+
 
 class ExtrinsicCalibrator(rclpy.node.Node):
   def __init__(self):
@@ -81,9 +61,31 @@ class ExtrinsicCalibrator(rclpy.node.Node):
     t.transform.rotation.y = quat[1]
     t.transform.rotation.z = quat[2]
     t.transform.rotation.w = quat[3]
-    print("sending new TF")
+    self.get_logger().info("sending new TF")
     self.tf_static_broadcaster.sendTransform(t)
   
+  def extrinsicsCompute(self, object_points_list: list, image_points_list: list, k_matrix: list):
+    object_points = np.array(object_points_list).reshape(len(object_points_list), 3, 1)
+    image_points = np.array(image_points_list).reshape(len(object_points_list), 2, 1)
+    camera_matrix = np.array(k_matrix).reshape(3, 3)
+    dist_coeffs = np.zeros((5,1))
+    ret_val, r_vec, t_vec = cv2.solvePnP(object_points, image_points, camera_matrix, dist_coeffs)
+    if(ret_val):
+      r_solved, _ = cv2.Rodrigues(r_vec)
+      t_vec = t_vec.reshape(3, 1)
+      homo_vec = np.array([[0.0, 0.0, 0.0, 1.0]])
+      tf_matrix = np.hstack((r_solved, t_vec))
+      tf_matrix = np.vstack((tf_matrix, homo_vec))
+      self.get_logger().info("transformation matrix:")
+      self.get_logger().info(f"[[{tf_matrix[0][0]}, {tf_matrix[0][1]}, {tf_matrix[0][2]}, {tf_matrix[0][3]}], \n[{tf_matrix[1][0]}, {tf_matrix[1][1]}, {tf_matrix[1][2]}, {tf_matrix[1][3]}], \n[{tf_matrix[2][0]}, {tf_matrix[2][1]}, {tf_matrix[2][2]}, {tf_matrix[2][3]}], \n[{tf_matrix[3][0]}, {tf_matrix[3][1]}, {tf_matrix[3][2]}, {tf_matrix[3][3]}]\n]")
+      r = R.from_matrix(r_solved)
+      quat = r.as_quat()
+      self.get_logger().info("quaternion: "+"[x, y, z, w]")
+      self.get_logger().info(f"[{quat[0]}, {quat[1]}, {quat[2]}, {quat[3]}]")
+      euler = r.as_euler('zyx')
+      self.get_logger().info(f"x: {t_vec[0][0]}\ny: {t_vec[1][0]}\nz: {t_vec[2][0]}\nroll: {euler[2]}\npitch: {euler[1]}\nyaw: {euler[0]}")
+      return quat, t_vec
+
   def cameraPointsCallback(self, msg: Float32MultiArray):
     if(len(msg.data)<2 and len(msg.data)/2==len(self.camera_points)):
       return
@@ -91,12 +93,12 @@ class ExtrinsicCalibrator(rclpy.node.Node):
     point.append(msg.data[-2])
     point.append(msg.data[-1])
     self.camera_points.append(point)
-    print("number of camera points: "+ str(len(self.camera_points)) + "  x: "+str(point[0]) + "  y: "+str(point[1]))
+    self.get_logger().info("number of camera points: "+ str(len(self.camera_points)) + "  x: "+str(point[0]) + "  y: "+str(point[1]))
   
   def objectPointsCallback(self, msg: PointStamped):
     point = [msg.point.x, msg.point.y, msg.point.z]
     self.object_points.append(point)
-    print("number of object points: "+ str(len(self.object_points)) + "  x: "+str(point[0]) + "  y: "+str(point[1]) + "  z: "+str(point[2]))
+    self.get_logger().info("number of object points: "+ str(len(self.object_points)) + "  x: "+str(point[0]) + "  y: "+str(point[1]) + "  z: "+str(point[2]))
     
   def timerCallback(self,):
     num_camera_points = len(self.camera_points)
@@ -105,9 +107,9 @@ class ExtrinsicCalibrator(rclpy.node.Node):
       if(num_camera_points > self.num_points):
         self.num_points += 1
         if(num_camera_points>=6):
-          print("calibrating...")
-          quat, t_vec = extrinsicsCompute(self.object_points, self.camera_points, self.K)
-          print("calibrated")
+          self.get_logger().info("calibrating...")
+          quat, t_vec = self.extrinsicsCompute(self.object_points, self.camera_points, self.K)
+          self.get_logger().info("calibrated")
           self.make_transforms(quat, t_vec)
 
 def main():
